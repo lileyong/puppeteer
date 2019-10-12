c1b180d094a45d14b9f71e86bb299c40ec6e368b
promise.li <promise.li@corp.to8to.com>
2019-10-12 10:10:12 +0800

    screenshot增加fullPage参数

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 const puppeteer = require('puppeteer')
-const url = 'https://www.jd.com/?cu=true&utm_source=baidu-pinzhuan&utm_medium=cpc&utm_campaign=t_288551095_baidupinzhuan&utm_term=0f3d30c8dba7459bb52f2eb5eba8ac7d_0_d8717cc9df334cbcb9c5d8a4cbc9610b'
+const url = 'https://www.to8to.com/zb/index11.html'
 
-async function savePage() {
-    const browser = await puppeteer.launch()
+async function savePage () {
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: 1920,
+            height: 600
+        }
+    })
     const page = await browser.newPage()
     await page.goto(url, {
         waitUntil: 'networkidle2'
     })
 
-    const dimensions = await page.evaluate(() => {
-        return {
-            scrollWidth: document.body.scrollWidth,
-            scrollHeight: document.body.scrollHeight
-        }
-    })
-    await page.setViewport({
-        width: dimensions.scrollWidth > 750 ? 1920 : 750,
-        height: dimensions.scrollHeight
-    })
-
     await page.screenshot({
-        path: 'dist/output.jpg'
+        path: 'dist/output.jpg',
+        fullPage: true
     })
     await browser.close()
 }
